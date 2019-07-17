89e4bcd6babfc10f4b1bd30e1f50145d25600dec
Jordan <jordanmittelman@gmail.com>
2019-07-17 13:54:03 -0400

    add apidoc.json and passport config files

---
+++ config/passport.js
@@ -0,0 +1,30 @@
+const passportJWT = require("passport-jwt");
+const ExtractJwt = passportJWT.ExtractJwt;
+const Strategy = passportJWT.Strategy;
+
+const config = require("./config");
+
+const User = require("../db/models/User");
+// const User = mongoose.model("User");
+
+const params = {
+  secretOrKey: config.jwtSecret,
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken()
+};
+
+module.exports = passport => {
+  passport.use(
+    new Strategy(params, (payload, done) => {
+      User.findById(payload.id)
+        .then(user => {
+          if (user) {
+            return done(null, {
+              id: user.id
+            });
+          }
+          return done(null, false);
+        })
+        .catch(err => console.error(err));
+    })
+  );
+};
