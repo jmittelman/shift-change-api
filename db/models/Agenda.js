b850b04ae11e0b79c658fb90df4b6411e2086c92
Jordan <jordanmittelman@gmail.com>
2019-07-21 17:50:22 -0400

    change models

--- db/models/Agenda.js
+++ db/models/Agenda.js
@@ -3,14 +3,16 @@ const mongoose = require("../connection.js");
 const Schema = mongoose.Schema;
 
 const Agenda = new Schema({
-  date: Date,
+  date: String,
   announcements: String,
   tasks: String,
   shoutOuts: String,
-  lesson: {
-    type: Schema.Types.ObjectId,
-    ref: "Lesson"
-  },
+  lesson: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Lesson"
+    }
+  ],
   author: {
     type: Schema.Types.ObjectId,
     ref: "User"
